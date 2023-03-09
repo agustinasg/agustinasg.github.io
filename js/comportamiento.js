0e125e12f2d0224226e32c6b945961fdee0a4797
agustinasg <126184230+agustinasg@users.noreply.github.com>
2023-03-08 22:31:43 -0300

    Add files via upload

---
+++ js/comportamiento.js
@@ -0,0 +1,10 @@
+
+console.log("Esperando envío de datos de contacto");
+
+
+document.getElementById("boton").addEventListener("click", function () {
+    console.log("Se enviaron los datos de contacto");
+    alert("Muchas gracias por dedicar tu tiempo a leer mi cv!");
+    document.body.style.backgroundColor = "#FFFFFF";
+})
+
